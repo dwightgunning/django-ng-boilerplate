cc007eef5725b992cd1d9f813401ae7247f778b6
Dwight Gunning <dwight@dwightgunning.com>
2017-08-05 16:54:54 +0200

    Setup angular app with Foundation Sites, Webfonts, Font-Awesome

--- ng-src/app/app.component.ts
+++ ng-src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import * as WebFont from 'webfontloader';
+
+declare let $: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Django Angular Boilerplate';
+  url = 'http://localhost:8000/api/';
+  apiResponse;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    console.log('AppComponent initializing...');
+    $(document).foundation();
+
+    WebFont.load({
+      google: {
+      families: ['Open+Sans']
+      }
+    });
+
+
+    this.getApi();
+  }
+
+  public getApi() {
+    this.http.get(this.url).toPromise().then((res) => {
+      this.apiResponse = res;
+      console.log(res);
+    });
+  }
 }
--- ng-src/app/app.module.ts
+++ ng-src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import * as Raven from 'raven-js';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+
+
+Raven
+  .config(environment.SENTRY_PUBLIC_DSN)
+  .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+
+  handleError(err: any): void {
+    Raven.captureException(err);
+  }
+
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
