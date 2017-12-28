fd0460b908e2bfbf19f86e7e5289464cdb18b34c
Dwight Gunning <dwight@dwightgunning.com>
2017-12-28 10:18:38 +0100

    Update frontend deps and improve Foundation integration

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'coverage-istanbul', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
