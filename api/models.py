0c602c77a882ae864dd482faa5848aa5bbb5be94
Dwight Gunning <dwight@dwightgunning.com>
2020-08-27 06:35:36 +0300

    Added a base timestamped model with created/updated fields

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class BaseTimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
