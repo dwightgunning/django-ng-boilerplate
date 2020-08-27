3fffdc5a593c82cb3be353a3250de31af2ad7e95
Dwight Gunning <dwight@dwightgunning.com>
2020-08-27 06:30:33 +0300

    Updated url routes to 'path' syntax for Django 2.0

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
 from api.views import api, user
 
 urlpatterns = [
-    url(r'^$', api.APIRootView.as_view(), name='APIRootView'),
-    url(r'^auth/login/$', obtain_jwt_token, name='login'),
-    url(r'^user/$', user.UserView.as_view(), name='user'),
+    path('', api.APIRootView.as_view(), name='APIRootView'),
+    path('auth/login/', obtain_jwt_token, name='login'),
+    path('user/', user.UserView.as_view(), name='user'),
 ]
